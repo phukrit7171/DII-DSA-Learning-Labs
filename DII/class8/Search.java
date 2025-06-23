9361588418d60205b6b75f79d512a23e415034a6
Phukrit Kittinontana <phukrit7171@gmail.com>
2025-06-23 15:37:10 +0700

    Update Search.java

--- DII/class8/Search.java
+++ DII/class8/Search.java
@@ -10,7 +10,10 @@ public class Search {
     }
 
     public static void BinarySearch(int[] a, int x) {
+        
         BinarySearch_recur(a, x, 0, a.length - 1);
+       
+
     }
 
     private static void BinarySearch_recur(int[] a, int x, int left, int right) {
@@ -21,7 +24,8 @@ public class Search {
 
         // Find the middle index
         int mid = left + (right - left) / 2;
-
+        
+        System.out.println("left: " + a[left] + ", right: " + a[right] + " mid: " + a[mid]);
         // Check if the middle element is the target
         if (a[mid] == x) {
             System.out.println("The number " + x + " is found at index " + mid);
@@ -39,24 +43,29 @@ public class Search {
     }
 
     public static void main(String[] args) {
-        // Q1
+        int[] a = { 7, 15, 20, 26, 38, 39, 42, 56, 58 };
+
         System.out.println("Q1: For Loop Search");
-        int[] a = { 6, 1, 2, 3, 4, 5, 0, 7, 8, 9 };
-        for (int i = 0; i <= 10; i++) {
-            int x = i;
-            System.out.print("Searching for " + x + ": ");
-            ForLoopSearch(a, x);
+
+        // Q1
+
+        for (int i : a) {
+            System.out.print("Searching for " + i + ": ");
+            ForLoopSearch(a, i);
         }
 
-
-        //Q2
+        // Q2
         System.out.println("\nQ2: Binary Search");
-        int[] a_sorted = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        for (int i = 9; i >= 0; i--) {
-            int x = i;
-            System.out.print("Searching for " + x + ": ");
-            BinarySearch(a_sorted, x);
+        for (int i: a) {
+
+            System.out.println("Searching for " + i + ": ");
+            
+            BinarySearch(a, i);
+            System.out.println("-------------------------");
+
+
         }
+
 
     }
 }
