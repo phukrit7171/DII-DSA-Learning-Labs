fed5f96e5ca49915cb6d9f0c428769dadcb3341b
Phukrit Kittinontana <phukrit7171@gmail.com>
2025-07-14 11:57:39 +0700

    refactor: Simplify comments in lab11 for clarity

--- DII/class11/lab11.java
+++ DII/class11/lab11.java
@@ -35,6 +35,7 @@ public class lab11 {
         
         // Test print_tree
         System.out.println("Original tree:");
+        System.out.println("root: " + root.get_value());
         tree1.print_tree(root);
         
         // Test is_leaf_node
@@ -47,18 +48,18 @@ public class lab11 {
         System.out.println("\nTree v2 (no leaf parents):");
         tree1.print_tree_v2(root);
         
-        // Test DFS and BFS (Depth-First Search and Breadth-First Search)
+        // Test DFS and BFS
         ((node) tree1.get_root().get_child().get(2)).set_value(5);
         System.out.println("\nAfter changing node 4 to 5:");
         tree1.print_tree_v2(root);
         
-        // Test DFS (Depth-First Search)
+        // Test DFS
         System.out.println("\nDFS test:");
         node node1 = tree1.dfs(root, 5);
         System.out.println("Found value: " + node1.get_value()); // 5
         System.out.println("Parent value: " + node1.get_parent().get_value()); // 2
         
-        // Test BFS (Breadth-First Search)
+        // Test BFS
         System.out.println("\nBFS test:");
         node node2 = tree1.bfs(root, 5);
         System.out.println("Found value: " + node2.get_value()); // 5
